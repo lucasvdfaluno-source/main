a182cf82cc2353ec751f691cb0e746c6c2df496e
lucas2023309080-sketch <lucas.2023309080@aluno.iffar.edu.br>
2026-05-11 18:18:02 -0300

    meu amor2
    segunda edição

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+# ROTA DO JOGO
+@app.route("/game")
+def game():
+    return render_template("game.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
